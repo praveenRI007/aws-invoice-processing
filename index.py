0268159191814bfbb5e906b7cbc794ca74aaf744
PraveenRI99 <praveenriemplfizh@gmail.com>
2026-04-14 00:00:29 +0100

    Update index.py

--- index.py
+++ index.py
@@ -25,7 +25,7 @@ app.add_middleware(
 
 # Config via App Runner Environment Variables
 AWS_REGION     = os.getenv("AWS_REGION", "us-east-1")
-S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "invoice-processing-2025")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "invoice-processing-2028")
 
 
 def get_s3_client():
